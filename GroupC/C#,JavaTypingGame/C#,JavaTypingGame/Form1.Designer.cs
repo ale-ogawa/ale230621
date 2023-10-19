41325e8ec2d11de0783c7150465c3128147dd744
sakata <sakatamanaka@gmail.com>
2023-10-19 16:43:30 +0900

    デザイン変更

--- GroupC/C#,JavaTypingGame/C#,JavaTypingGame/Form1.Designer.cs
+++ GroupC/C#,JavaTypingGame/C#,JavaTypingGame/Form1.Designer.cs
@@ -28,73 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.title = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.loginButton = new System.Windows.Forms.Button();
             this.newRegistrationButton = new System.Windows.Forms.Button();
             this.endButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // title
-            // 
-            this.title.Font = new System.Drawing.Font("ＭＳ 明朝", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.title.Location = new System.Drawing.Point(147, 36);
-            this.title.Name = "title";
-            this.title.Size = new System.Drawing.Size(495, 91);
-            this.title.TabIndex = 2;
-            this.title.Text = "title";
-            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // loginButton
             // 
-            this.loginButton.Location = new System.Drawing.Point(291, 145);
+            this.loginButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("loginButton.BackgroundImage")));
+            this.loginButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.loginButton.Location = new System.Drawing.Point(291, 164);
             this.loginButton.Name = "loginButton";
             this.loginButton.Size = new System.Drawing.Size(210, 65);
             this.loginButton.TabIndex = 3;
-            this.loginButton.Text = "ログイン";
             this.loginButton.UseVisualStyleBackColor = true;
             this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
             // 
             // newRegistrationButton
             // 
-            this.newRegistrationButton.Location = new System.Drawing.Point(291, 227);
+            this.newRegistrationButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("newRegistrationButton.BackgroundImage")));
+            this.newRegistrationButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.newRegistrationButton.Location = new System.Drawing.Point(291, 261);
             this.newRegistrationButton.Name = "newRegistrationButton";
             this.newRegistrationButton.Size = new System.Drawing.Size(210, 65);
             this.newRegistrationButton.TabIndex = 4;
-            this.newRegistrationButton.Text = "新規登録";
             this.newRegistrationButton.UseVisualStyleBackColor = true;
             this.newRegistrationButton.Click += new System.EventHandler(this.newRegistrationButton_Click);
             // 
             // endButton
             // 
-            this.endButton.Location = new System.Drawing.Point(291, 314);
+            this.endButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("endButton.BackgroundImage")));
+            this.endButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.endButton.Location = new System.Drawing.Point(291, 352);
             this.endButton.Name = "endButton";
             this.endButton.Size = new System.Drawing.Size(210, 65);
             this.endButton.TabIndex = 5;
-            this.endButton.Text = "終了";
             this.endButton.UseVisualStyleBackColor = true;
             this.endButton.Click += new System.EventHandler(this.endButton_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(189, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(446, 124);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(213)))), ((int)(((byte)(62)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.endButton);
             this.Controls.Add(this.newRegistrationButton);
             this.Controls.Add(this.loginButton);
-            this.Controls.Add(this.title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label title;
         private System.Windows.Forms.Button loginButton;
         private System.Windows.Forms.Button newRegistrationButton;
         private System.Windows.Forms.Button endButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
