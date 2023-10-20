3700ca749b5d9114257856ba4bfa75c3215ae942
koyama <koyama@dummy.com>
2023-10-20 17:34:10 +0900

    ■更新内容 ・Gacha.csを追加。ガチャ結果がリストとして入る想定のGachaクラスです。

---
+++ GroupE/GachaSimulator/GachaSimulator/Gacha.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GachaSimulator
+{
+    class Gacha
+    {
+        public int Id { get; set; }
+        public string Rarity { get; set; }
+        public string Name { get; set; }
+
+        public Gacha(int id,string rarity,string name)
+        {
+            Id = id;
+            Rarity = rarity;
+            Name = name;
+        }
+    }
+}
