f3d2b5f576163bae8a17237d37045f1e0a9e58f8
shiomi <shiomiasa@mail>
2023-10-20 17:12:53 +0900

    ・獲得ガチャ表示画面　csvファイルを読み込み、データグリッドビューに表示させる為のコード

--- GroupE/GachaSimulator/GachaSimulator/ItemListForm.cs
+++ GroupE/GachaSimulator/GachaSimulator/ItemListForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace GachaSimulator
 {
@@ -15,6 +16,34 @@ namespace GachaSimulator
         public ItemListForm()
         {
             InitializeComponent();
+
+            string filePath = @"C:\gachacsv\gachasetting2.csv";
+
+            // CSVファイルをDataTableに読み込む
+            DataTable dt = new DataTable();
+            using (StreamReader rd = new StreamReader(filePath))
+            {
+                // 最初の行を読み込んで列数を取得
+                string[] firstLine = rd.ReadLine().Split(',');
+                int numberOfColumns = firstLine.Length;
+
+                dt.Columns.Add("ID");
+                dt.Columns.Add("レア度");
+                dt.Columns.Add("名前");
+
+                dt.Rows.Add(firstLine);
+
+                while (!rd.EndOfStream)
+                {
+                    string[] data = rd.ReadLine().Split(',');
+                    dt.Rows.Add(data);
+                }
+
+                // データグリッドビューにデータテーブルをバインド
+                dataGridView1.DataSource = dt;
+
+            }
+
         }
 
         private void returnTopButton_Click(object sender, EventArgs e)
